f7046f0e589ae29c65da4791b0c2aaba415f62c4
Johannes Reiter <johannes@reiter.work>
2020-04-04 12:02:11 +0200

    [TASK] added Model Item and Model Shoppinglist, basic routing

--- server/bookstoreServer/database/seeds/DatabaseSeeder.php
+++ server/bookstoreServer/database/seeds/DatabaseSeeder.php
@@ -12,5 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $this->call(ShoppingListTableSeeder::class);
+        $this->call(ItemTableSeeder::class);
+
     }
 }
--- server/bookstoreServer/routes/api.php
+++ server/bookstoreServer/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\ShoppingList;
 use Illuminate\Http\Request;
 
 /*
@@ -15,4 +16,28 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('shoppinglist/{id}', function ($id) {
+    try {
+        return App\ShoppingList::getListById($id);
+    } catch (Error $e) {
+        return $e->getMessage();
+    }
+});
+
+Route::post('shoppinglist', function ($id) {
+    try {
+        return App\ShoppingList::createList();
+    } catch (Error $e) {
+        return $e->getMessage();
+    }
+});
+
+Route::put('shoppinglist/{id}', function ($id) {
+    try {
+        return App\ShoppingList::updateList($id);
+    } catch (Error $e) {
+        return $e->getMessage();
+    }
 });
